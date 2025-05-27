263fdc31102ae940e65698d696526ca9a946aec5
조유진 <s2570@e-mirim.hs.kr>
2025-05-27 11:27:28 +0900

    fix: remove unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import './App.css';
 import { FiGithub, FiMail, FiInstagram, FiLinkedin } from 'react-icons/fi';
 import { SiNotion } from 'react-icons/si';
